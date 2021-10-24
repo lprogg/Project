755e5fac4c7d7745e2e0d3e5f86e2ca8ef788c1f
lprogg <lprogg>
2021-10-24 14:15:55 +0300

    Change project name

--- src/js/appController.js
+++ src/js/appController.js
@@ -71,7 +71,7 @@ define(['knockout', 'ojs/ojcontext', 'ojs/ojmodule-element-utils', 'ojs/ojknocko
 
       // Header
       // Application Name used in Branding Area
-      this.appName = ko.observable("App Name");
+      this.appName = ko.observable("Charts");
       // User Info used in Global Navigation area
       this.userLogin = ko.observable("john.hancock@oracle.com");
 
